e7b759370b01cbde16a9576054705392225b0051
Jennifer Li <jzli@hmc.edu>
2022-04-04 13:17:17 -0700

    uncommented normalized code

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -9,12 +9,10 @@ for file in $files; do
     time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:11013/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
-<<COMMENT
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:21013/ --inputs $files
-COMMENT
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
